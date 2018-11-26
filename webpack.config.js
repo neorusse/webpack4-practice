5fc6ba8ad81f0fb9d519bb1ebefc29477b7da798
Russell Nyorere <ecodenhub@gmail.com>
2018-11-26 14:53:56 +0100

    Loaders configuration added

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,36 @@
 const path = require('path');
 
 module.exports = {
-    entry: './src/index.js',
-    output: {
-        filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist')
-    }
+  entry: './src/index.js',
+  output: {
+      filename: 'bundle.js',
+      path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+        {
+            test: /\.js$/,
+            exclude: /(node_modules|bower_components)/,
+            use: {
+            loader: 'babel-loader',
+            }
+        },
+        {
+          test: /\.scss$/,
+          use: [{
+            loader: 'style-loader',
+          }, {
+            loader: 'css-loader',
+          }, {
+              loader: 'sass-loader',
+          }]
+        },
+        {
+          test: /\.(png|svg|jpg|gif)$/,
+          use: {
+          loader: 'file-loader',
+          }
+      },
+    ]
+  }
 }
