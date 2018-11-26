ec16acd27d81e106fde7b0f558509df4d74b7a67
Russell Nyorere <ecodenhub@gmail.com>
2018-11-26 16:09:15 +0100

    File added to implement code spliting

---
+++ src/app.js
@@ -0,0 +1,11 @@
+import './app.scss'
+
+const button = document.createElement("button")
+button.textContent = 'Open chat'
+document.body.appendChild(button)
+
+button.onclick = () => {
+  import(/* webpackChunkName: "chat" */ "./chat").then(chat => {
+    chat.init()
+  })
+}
