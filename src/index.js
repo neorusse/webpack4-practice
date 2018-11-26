7c2f6bfda620afc3229ee275d0a68812cc0dcc17
Russell Nyorere <ecodenhub@gmail.com>
2018-11-26 13:01:04 +0100

    lodash-es and employee.js file imported and consumed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
+import { groupBy } from "lodash-es";
+import employee from "./employee";
+
+const managerGroups = groupBy(employee, "manager");
+
 const root = document.createElement('div');
-root.innerHTML = `<p>Practicing Webpack 4 and Module Bundling</p>`;
+root.innerHTML = `<pre>${JSON.stringify(managerGroups, null, 2)}</pre>`;
 document.body.appendChild(root);
